38399c992963e3ebf2ddc42a990b7859a96458c4
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2024-11-01 15:55:55 +0200

    Update BookDao.java

--- src/main/java/Dao/BookDao.java
+++ src/main/java/Dao/BookDao.java
@@ -1,4 +1,33 @@
 package Dao;
 
+import model1.Book;
+import model1.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.UUID;
+
 public class BookDao {
+
+
+    public String saveBook(Book book) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return "Data Saved Successfully";
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public Book findBookById(UUID bookId) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Book.class, bookId);
+        }
+    }
 }
