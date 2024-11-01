6a402dc8f07351c31a9bf1310a1b450c958dbdaa
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2024-11-01 17:16:06 +0200

    Update BorrowerDao.java

--- src/main/java/Dao/BorrowerDao.java
+++ src/main/java/Dao/BorrowerDao.java
@@ -1,4 +1,25 @@
 package Dao;
 
+import model1.Borrower;
+import model1.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class BorrowerDao {
+
+    public String borrowBook(Borrower borrower) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(borrower);
+            transaction.commit();
+            return "Book borrowed successfully.";
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return "Error borrowing book: " + e.getMessage();
+        }
+    }
 }
