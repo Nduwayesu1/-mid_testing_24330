c5e336127b70f13c72474e31e03622d6773d5150
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2024-11-09 18:50:24 +0200

    Update Book.java

--- src/main/java/Models/Book.java
+++ src/main/java/Models/Book.java
@@ -1,37 +1,119 @@
-package Models;
+package model1;
 
-import Enums.Book_status;
+import model1.Enum.EBook_status;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne; // Importing ManyToOne
-import javax.persistence.OneToMany; // Importing OneToMany
-import java.util.Date;
-import java.util.List;
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.UUID;
-
 @Entity
+@Table(name = "book")
 public class Book {
-
     @Id
-    private UUID book_Id;
-    private int edition;
-    private String ISBNcode;
-    private Date publication_year_date;
-    private String publisher_name;
+    @Column(name = "book_id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID bookId;
 
     @Enumerated(EnumType.STRING)
-    private Book_status book_status;
+    @Column(name = "status", nullable = false)
+    private EBook_status status;
 
-    @ManyToOne // Establishing Many-to-One relationship with Shelf
+    @Column(name = "edition", nullable = false)
+    private Integer edition;
+
+    @Column(name = "isbn_code", nullable = false, unique = true)
+    private String isbnCode;
+
+    @Column(name = "publication_year", nullable = false)
+    private LocalDate publicationYear;
+
+    @Column(name = "publisher_name", nullable = false)
+    private String publisherName;
+
+    @ManyToOne
+    @JoinColumn(name = "shelf_id", referencedColumnName = "shelf_id", nullable = false)
     private Shelf shelf;
 
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @OneToMany(mappedBy = "book")
-    private List<Borrower> borrowers;
+    public Book() {
+    }
 
+    public Book(UUID bookId, EBook_status status, Integer edition, String isbnCode, LocalDate publicationYear, String publisherName, Shelf shelf, String title) {
+        this.bookId = bookId;
+        this.status = status;
+        this.edition = edition;
+        this.isbnCode = isbnCode;
+        this.publicationYear = publicationYear;
+        this.publisherName = publisherName;
+        this.shelf = shelf;
+        this.title = title;
+    }
 
+    public Book(UUID bookId) {
+    }
+
+    public UUID getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(UUID bookId) {
+        this.bookId = bookId;
+    }
+
+    public EBook_status getStatus() {
+        return status;
+    }
+
+    public void setStatus(EBook_status status) {
+        this.status = status;
+    }
+
+    public Integer getEdition() {
+        return edition;
+    }
+
+    public void setEdition(Integer edition) {
+        this.edition = edition;
+    }
+
+    public String getIsbnCode() {
+        return isbnCode;
+    }
+
+    public void setIsbnCode(String isbnCode) {
+        this.isbnCode = isbnCode;
+    }
+
+    public LocalDate getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(LocalDate publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public Shelf getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(Shelf shelf) {
+        this.shelf = shelf;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
