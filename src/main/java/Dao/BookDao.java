c791dccbaaca9221bdbb56ee78f35e460a03d1f2
Nduwayesu1 <99731288+Nduwayesu1@users.noreply.github.com>
2024-11-09 18:42:17 +0200

    Update BookDao.java

--- src/main/java/Dao/BookDao.java
+++ src/main/java/Dao/BookDao.java
@@ -4,7 +4,11 @@ import model1.Book;
 import model1.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.UUID;
 
 public class BookDao {
@@ -14,7 +18,7 @@ public class BookDao {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            session.persist(book);
+            session.merge(book);
             transaction.commit();
             return "Data Saved Successfully";
         } catch (Exception ex) {
@@ -29,5 +33,19 @@ public class BookDao {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.get(Book.class, bookId);
         }
+    }
+
+    public int getTotalNumberOfBooks() {
+        int count = 0;
+        String hql = "SELECT COUNT(b) FROM Book b"; // Adjust 'Book' to match your entity class name
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Long> query = session.createQuery(hql, Long.class);
+            count = query.uniqueResult().intValue();
+        } catch (Exception e) {
+            System.out.println("Error retrieving total number of books: " + e.getMessage());
+        }
+
+        return count;
     }
 }
